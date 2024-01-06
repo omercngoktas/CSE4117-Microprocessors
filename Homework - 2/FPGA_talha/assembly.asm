a9151aae012f79c6cc28834437147d7cc053d5de
Ömercan Göktaş <77095758+omercngoktas@users.noreply.github.com>
2024-01-06 12:50:03 +0300

    update

--- Homework - 2/FPGA_talha/assembly.asm
+++ Homework - 2/FPGA_talha/assembly.asm
@@ -1,13 +1,12 @@
 .data
-.code       
- loop    ldi 0 0x901
-         ldi 5 0x0001
-         ld 1 0
-         and 3 1 5
-         jz loop
-         ldi 0 0x900
-         ld  0 0
-         ldi 2 0xb00
-         add 6 0 5
-         st 2 6
-         jmp loop
+.code   
+loop	ldi 0 0x901
+	ldi 5 0x0001
+	ld 1 0
+	and 3 1 5
+	jz loop
+	ldi 5 0xb00
+	ldi 2 0x900
+	ld 2 2
+	st 5 2
+	jmp loop
--- Homework - 2/FPGA_talha/sevensegment.sv
+++ Homework - 2/FPGA_talha/sevensegment.sv
@@ -20,36 +20,36 @@ always_ff @(posedge clk)
 
 always_comb
     case(data[count])
-        0:display=7'b1111110; //starts with a, ends with g
-        1:display=7'b0110000;
-        2:display=7'b1101101;
-        3:display=7'b1111001;
-        4:display=7'b0110011;
-        5:display=7'b1011011;
-        6:display=7'b1011111;
-        7:display=7'b1110000;
-        8:display=7'b1111111;
-        9:display=7'b1111011;
-        'ha:display=7'b1110111;
-        'hb:display=7'b0011111;
-        'hc:display=7'b1001110;
-        'hd:display=7'b0111101;
-        'he:display=7'b1001111;
-        'hf:display=7'b1000111;
-        default display=7'b1111111;
+			0: display=7'b0000001;
+			1: display=7'b1001111;
+			2: display=7'b0010010;
+			3: display=7'b0000110;
+			4: display=7'b1001100;
+			5: display=7'b0100100;
+			6: display=7'b0100000;
+			7: display=7'b0001111;
+			8: display=7'b0000000;
+			9: display=7'b0000100;
+			'ha: display=7'b0001000;
+			'hb: display=7'b1100000;
+			'hc: display=7'b0110001;
+			'hd: display=7'b1000010;
+			'he: display=7'b0110000;
+			'hf: display=7'b0111000;
+			default: display=7'b0000000;
     endcase
 
   always_comb
     begin
-    data[0]=din[15:12];
-    data[1]=din[11:8];
-    data[2]=din[7:4];
-    data[3]=din[3:0];
+    data[3]=din[15:12];
+    data[2]=din[11:8];
+    data[1]=din[7:4];
+    data[0]=din[3:0];
     end
 
 initial begin
     count = 2'b0;
-    grounds=4'b1110;
+    grounds=4'b0001;
     clk1=0;
 end
 
